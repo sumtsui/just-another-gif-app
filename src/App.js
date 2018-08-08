9cc1a386fc3805d7396991487af2c08d4e79fc12
Sum Tsui <sumtsui@gmail.com>
2018-08-08 17:39:32 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Trending from './components/trending';
 import Random from './components/random';
 import SearchForm from './components/searchForm';
 import SearchResult from './components/searchResult';
-const APIKey = process.env.REACT_APP_APIKey || require('./config');
+const APIKey = process.env.REACT_APP_APIKey;
 // const APIKey = require('./config');
 
 class App extends Component {
